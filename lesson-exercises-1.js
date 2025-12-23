81931914954993e3fff97ab4907255c9fa5b32c2
Kolibra <kkoliba@gmail.com>
2025-12-23 02:36:59 -0800

    Lesson 1: Exercises

---
+++ lesson-exercises-1.js
@@ -0,0 +1,61 @@
+
+/* Question 1a: 
+Use alert(...); to display 'Good morning!' in a popup.
+*/
+
+alert('Good morning');
+
+
+/* Question 1b: 
+Display your name in a pop up.
+*/
+
+alert('Kolibra');
+
+/* Question 1c: 
+Using math, calculate 10+5 in the Console.
+*/
+
+10+5;
+
+/* Question 1d: 
+Calculate 20-5 in the Console.
+*/
+
+20-5;
+
+/* Question 1e: 
+Calculate 2+2-5 in the Console.
+*/
+
+2+2-5;
+
+/* Question 1f: 
+Use document.body.innerHTML = ...; to display 'Good morning!' on the web page.
+*/
+
+document.body.innerHTML = 'Good morning!';
+
+/* Question 1g: 
+Display your name on the web page.
+*/
+
+document.body.innerHTML = 'Kolibra';
+
+/* Question 1h: 
+You order a T-shirt for $10, socks or $8, and dinner plates for $20. Use JavaScript to calculate the total cost of your order. 
+*/
+
+10+8+20;
+
+/* Question 1i: 
+Your bank account has $100, you spend $20 on unch, $50 on dinner, and earn $200 from your job. Calculate how much money you have.
+*/
+
+(200+100) - (20+50);
+
+/* Question 1j: 
+Use document.body.innerHTML = ...; to make the web page blank.
+*/
+
+document.body.innerHTML = '';s
