2e899bd1822e617275dca6956334a1d1ba7330c2
Kolibra <kkoliba@gmail.com>
2026-04-11 16:50:14 -0700

    Lesson 11: Loop Project

--- scripts/lesson11_project_loop.js
+++ scripts/lesson11_project_loop.js
@@ -1,4 +1,17 @@
-const todoList = [];
+const todoList = ["Buy groceries", "Walk the dog", "Read a book"];
+renderTodoList();
+function renderTodoList() {
+  let todoListHTML = "";
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+  }
+
+  const todoListElement = document.querySelector(".js-todo-list");
+  todoListElement.innerHTML = todoListHTML;
+}
 
 function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
@@ -7,4 +20,5 @@ function addTodo() {
   todoList.push(name);
 
   console.log(todoList);
+  renderTodoList();
 }
