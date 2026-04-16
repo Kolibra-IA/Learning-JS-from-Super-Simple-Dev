3d26f8fb4921812621dde37651544e3234021efd
Kolibra <kkoliba@gmail.com>
2026-04-16 11:07:55 -0700

    Lesson Exercise: 11m

---
+++ lesson11-exercises/lessonm.js
@@ -0,0 +1,22 @@
+function minMax(x) {
+  let minMaxObj;
+  if (x == []) {
+    minMaxObj = { min: null, max: null };
+    return minMaxObj;
+  }
+
+  minMaxObj = { min: x[0], max: x[0] };
+
+  for (i = 0; i < x.length; i++) {
+    if (x[i] < minMaxObj.min) {
+      minMaxObj.min = x[i];
+    } else if (x[i] > minMaxObj.max) {
+      minMaxObj.max = x[i];
+    }
+  }
+
+  return minMaxObj;
+}
+
+console.log(minMax([0, 9, 4, 5, 8, 2, 19, -4]));
+console.log(minMax([]));
