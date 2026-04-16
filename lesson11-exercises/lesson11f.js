35d348fb9e9169646bc7869aee11cee820c2c24d
Kolibra <kkoliba@gmail.com>
2026-04-16 10:57:16 -0700

    Lesson Exercise: 11f

---
+++ lesson11-exercises/lesson11f.js
@@ -0,0 +1,23 @@
+let count = 0;
+
+let i = 0;
+
+while (i <= 10) {
+  if (i % 2 === 0 && i != 0) {
+    count++;
+  }
+  i++;
+}
+
+console.log(count);
+
+let count2 = 0;
+
+let i2 = 5;
+
+while (i2 >= 0) {
+  count2++;
+  i2--;
+}
+
+console.log(count2);
