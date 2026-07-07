1220118464ebc9f73efc52476d76da8b7e42a9ea
Kolibra <kkolibra@gmail.com>
2026-07-07 11:00:25 -0700

    Exercise 15d: Completed

--- lesson15-exercises/15d.js
+++ lesson15-exercises/15d.js
@@ -1,3 +1,3 @@
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 
-document.querySelector("body").innerHTML = `<p>${dayjs().format("")}</p>`;
+document.querySelector("body").innerHTML = `<p>${dayjs().format("dddd")}</p>`;
